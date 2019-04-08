b5c4bcdb86ce4e8eae50a010b6b391f364649329
Michelle Salgado Calquín <nevinnostt@gmail.com>
2019-04-08 08:44:08 -0400

    Archivo de test que no tenía

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,13 +1,15 @@
+//Voy linea por linea, en los parámetros 
 describe('cipher', () => {
 
+//Esto es lo que yo quiero que devuelva, lo que espero que haga mi objeto
   it('debería ser un objeto', () => {
     assert.equal(typeof cipher, 'object');
   });
 
   describe('cipher.encode', () => {
-
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.encode, 'function');
+// 
+  it('debería ser una función', () => {
+    assert.equal(typeof cipher.encode, 'function');
     });
 
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
@@ -15,11 +17,11 @@ describe('cipher', () => {
 
   describe('cipher.decode', () => {
 
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.decode, 'function');
-    });
+  it('debería ser una función', () => {
+    assert.equal(typeof cipher.decode, 'function');
+  });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+  it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
   });
 
 });
